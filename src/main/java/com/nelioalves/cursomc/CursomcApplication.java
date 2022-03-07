813e464525884ff2b31501f59b1f906b5ed83a78
Daniel Coccia <daniel_coccia@hotmail.com>
2022-03-07 17:02:25 -0300

    Endpoint Clientes disponivel

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -68,12 +68,12 @@ public class CursomcApplication implements CommandLineRunner{
 		Estado est1 = new Estado(null, "São Paulo");
 		Estado est2 = new Estado(null, "Minas Gerais");
 		
-		Cidade c1 = new Cidade (null, "Uberlandia", est1);
-		Cidade c2 = new Cidade (null, "São Paulo", est2);
-		Cidade c3 = new Cidade (null, "Campinas", est2);
+		Cidade c1 = new Cidade (null, "Uberlandia", est2);
+		Cidade c2 = new Cidade (null, "São Paulo", est1);
+		Cidade c3 = new Cidade (null, "Campinas", est1);
 		
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		est1.getCidades().addAll(Arrays.asList(c2,c3));
+		est2.getCidades().addAll(Arrays.asList(c1));
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
